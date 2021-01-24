458809851e51a61935ccbf6598d011efa58caf27
james <omagasohe@gmail.com>
2021-01-24 01:46:42 +0000

    Added server files from pi

--- websocket.py
+++ websocket.py
@@ -10,6 +10,8 @@ import sys
 import RPi.GPIO as GPIO
 from hx711 import HX711
 
+hx = HX711(5, 6)
+
 hx.set_reading_format("MSB", "MSB")
 hx.set_reference_unit(377)
 
@@ -21,17 +23,26 @@ hx.tare()
 ReadList = []
 
 async def the_socket(websocket, path):
+    if path.endswith('/'):
+        path = path[:-1]
+    print(path)
     try:
-        while True:
-            await websocket.send(str(sum(ReadList)/len(ReadList)))
-            await asyncio.sleep(1)
-    except  websockets.exceptions.ConnectionClosed :
-        print ("closed")   
+        if path == "/tare":
+            hx.tare()
+            await websocket.send("tare complete")
+            await websocket.close()
+        else:
+            while True:
+                await websocket.send(str(sum(ReadList)/len(ReadList)))
+                await asyncio.sleep(1)
+
+    except  websockets.exceptions.ConnectionClosed:
+    	pass
 
 async def read_scale():
     global ReadList
     while True:
-        ReadList.append(hx.get_weight(5))
+        ReadList.append(0-hx.get_weight(5))
         if len(ReadList) > 5 :
             ReadList.pop(0)
         hx.power_down()
@@ -39,7 +50,7 @@ async def read_scale():
 
         await asyncio.sleep(0.2)
 
-start_server = websockets.serve(the_socket, "localhost", 8765)
+start_server = websockets.serve(the_socket, "192.168.0.184", 8765)
 
 asyncio.get_event_loop().run_until_complete(asyncio.wait([   
    read_scale(),
